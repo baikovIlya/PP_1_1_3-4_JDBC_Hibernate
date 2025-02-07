04923c05ed4774cbc57d9666b1174b34e11d26d0
baikovIlya <baikusha23@gmail.com>
2025-02-07 10:21:41 +0300

    implementation of the class Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String CONNECTION_URL = ConnectionData.MYSQL.getUrl();
+    private static final String USERNAME = ConnectionData.MYSQL.getUsername();
+    private static final String PASSWORD = ConnectionData.MYSQL.getPassword();
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException(e.getMessage());
+        }
+        return DriverManager.getConnection(CONNECTION_URL, USERNAME, PASSWORD);
+    }
 }
