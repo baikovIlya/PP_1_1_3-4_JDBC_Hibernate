e78641a51244a205a8899346fa6e7b8b470b3454
baikovIlya <baikusha23@gmail.com>
2025-02-10 17:31:04 +0300

    fix main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,8 +1,8 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.*;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.sql.SQLException;
 import java.util.Arrays;
@@ -10,22 +10,22 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        UserDao userDao = new UserDaoJDBCImpl();
-        userDao.createUsersTable();
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
         User user1 = new User("Ilya", "Baykov", (byte)31);
         User user2 = new User("Alena", "Manzhina", (byte)31);
         User user3 = new User("Ivan", "Ivanov", (byte)40);
         User user4 = new User("Vasiliy", "Petrov", (byte)50);
         List<User> inputUsers = Arrays.asList(user1, user2, user3, user4);
         for (User user : inputUsers) {
-            userDao.saveUser(user.getName(), user.getLastName(), user.getAge());
+            userService.saveUser(user.getName(), user.getLastName(), user.getAge());
             System.out.printf("User с именем — %s добавлен в базу данных\n", user.getName());
         }
-        List<User> usersFromDB = userDao.getAllUsers();
+        List<User> usersFromDB = userService.getAllUsers();
         for (User user : usersFromDB) {
             System.out.println(user);
         }
-        userDao.cleanUsersTable();
-        userDao.dropUsersTable();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
