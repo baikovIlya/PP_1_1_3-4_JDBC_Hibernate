13f339f953b4ecd7eb241baa9c0e84d246ea8fab
baikovIlya <baikusha23@gmail.com>
2025-02-10 15:07:54 +0300

    add Hibernate configuration to Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,18 +3,54 @@ package jm.task.core.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+import java.util.Properties;
+import jm.task.core.jdbc.model.User;
 
 public class Util {
     private static final String CONNECTION_URL = ConnectionData.MYSQL.getUrl();
     private static final String USERNAME = ConnectionData.MYSQL.getUsername();
     private static final String PASSWORD = ConnectionData.MYSQL.getPassword();
+    private static final String DRIVER = ConnectionData.MYSQL.getDriver();
+    private static final String DIALECT = ConnectionData.MYSQL.getDialect();
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnection() throws SQLException {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(DRIVER);
         } catch (ClassNotFoundException e) {
             throw new SQLException(e.getMessage());
         }
         return DriverManager.getConnection(CONNECTION_URL, USERNAME, PASSWORD);
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, DRIVER);
+                settings.put(Environment.URL, CONNECTION_URL);
+                settings.put(Environment.USER, USERNAME);
+                settings.put(Environment.PASS, PASSWORD);
+                settings.put(Environment.DIALECT, DIALECT);
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "none");
+
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
     }
 }
